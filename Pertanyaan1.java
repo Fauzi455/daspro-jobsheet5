bd140f48b45251983b6531b627780255b372fd0f
Fauzi455 <fauzirhmn45@gmail.com>
2024-09-27 10:12:41 +0700

    Pertanyaan1

---
+++ Pertanyaan1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    public class Pertanyaan1 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+System.out.print("Masukkan sebuah angka: ");
+int angka = sc.nextInt();
+
+String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+
+System.out.println("Angka " + angka + " " + hasil);
+        }
+    
+}
